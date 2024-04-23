a45be929ed12a594597c3ad15a17b863763001b7
parth <darkcodeing0@gmail.com>
2024-04-23 18:20:07 +0530

    day1 shorting

---
+++ Shorting/BubbleShort.java
@@ -0,0 +1,18 @@
+package Shorting;
+public class BubbleShort {
+    public static void main(String[] args) {
+        int []arr={9,8,7,6,5,4,3,2,1};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for (int e : arr) {
+            System.out.println(e);
+        }
+    }
+}
